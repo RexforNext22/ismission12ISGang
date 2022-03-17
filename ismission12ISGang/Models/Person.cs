027b14f42ff407a7e308a034a44ff36298654e95
Jacob Poor <jacob.poor98@gmail.com>
2022-03-17 12:17:23 -0600

    Jacob

---
+++ ismission12ISGang/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ismission12ISGang.Models
+{
+    public class Person
+    {
+        [Key]
+        [Required]
+        public int PersonID { get; set; }
+
+        [Required]
+        public int TimeID { get; set; }
+        public Time Time { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1,15)]
+        public int Size { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
