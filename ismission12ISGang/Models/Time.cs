81a5808422ef18555098851e30fac2497c057add
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-03-17 12:09:20 -0600

    Added by Ryan

---
+++ ismission12ISGang/Models/Time.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ismission12ISGang.Models
+{
+    public class Time
+    {
+        [Required]
+        [Key]
+        public int TimeID { get; set; }
+
+        [Required]
+        public string Month { get; set; }
+
+        [Required]
+        public string Day { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string TimeOfDay { get; set; }
+
+    }
+}
